99309b6c91703bb0c645a4f5bb8d810db057cb13
NitaRay <superlightwing@gmail.com>
2020-09-03 20:02:19 +0700

    Added integrity check before starting the bot

--- edubot.js
+++ edubot.js
@@ -11,10 +11,40 @@ const botClient = new Discord.Client();
 const token = process.env.BOT_TOKEN;
 const channelsPath = process.env.CHANNELS_ID_FILE_PATH;
 
-var channelsID = [];
+var channelsID = new Set();
 var announcementNumber = process.env.STARTING_ANNOUNCEMENT_NUMBER;
 
-const checkInterval = 60 * 60 * 1000;
+const checkInterval = process.env.CHECK_INTERVAL * 60 * 1000;
+
+function checkDirIntegrity() {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync('.env')) {
+            reject('Please create a .env with appropriate values as shown in the readme.md file!');
+            return;
+        }
+        if (!fs.existsSync(channelsPath)) {
+            try {
+                fs.openSync(filePath, 'a');
+                console.log('Created a file to store channel ids!');
+            } catch(err) {
+                console.error(err);
+                reject('Error encountered while trying to create a file!');
+                return;
+            }
+        }
+        if (!fs.existsSync(process.env.SCREENSHOTS_DIR_PATH))
+            fs.mkdir(SCREENSHOTS_DIR_PATH, (err) => {
+                if (err) {
+                    console.error(err);
+                    reject('Error encounterd while trying to create a folder');
+                    return;
+                }
+                else
+                    console.log('Created a folder to store screenshots!');
+            })
+        resolve('Directory integrity is good to start!');
+    })
+}
 
 function restartBot(channel) {
     channel.send('Restarting...')
@@ -24,17 +54,6 @@ function restartBot(channel) {
 
 function startBot() {
     return botClient.login(token);
-}
-
-function getChannelIDs(filePath) {
-    try {
-        if (!fs.existsSync(filePath))
-            fs.openSync(filePath, 'a');
-        return load_channels(filePath);
-    }
-    catch(err) {
-        console.error(err);
-    }
 }
 
 function checkAnnounement() {
@@ -60,7 +79,7 @@ function announce(announcement) {
     console.log(`Announced new message at ${new Date()}`);
 }
 
-function addChannel(guild, channel) {
+function addChannel(channel) {
     if (!channelsID.find((id) => id == channel.id)) {
         channelsID.push(channel.id);
         save_channels(channelsPath, channelsID);
@@ -72,13 +91,28 @@ function addChannel(guild, channel) {
     }
 }
 
+function removeChannel(channel) {
+    if (channelsID.find((id) => id == channel.id)) {
+        channelsID.delete(id);
+        save_channels(channelsPath, channelsID);
+        console.log('Removed a channel with id ' + channel.id);
+        channel.send('Successfully removed this channel from the announcement list!');
+    } else {
+        channel.send('This channel is not in the announcement list!');
+        console.log('Tried removing a non-existing channel ID.');
+    }
+}
+
 botClient.on('message', msg => {
     switch (msg.content.toLowerCase()) {
         case '.restart':
             restartBot(msg.channel)
             break;
         case '.addchannel':
-            addChannel(msg.guild, msg.channel);
+            addChannel(msg.channel);
+            break;
+        case '.removechannel':
+            removeChannel(msg.channel);
             break;
         case '.konami':
             msg.reply(`The next annoucement number is ${announcementNumber}`);
@@ -88,10 +122,12 @@ botClient.on('message', msg => {
 
 botClient.on('ready', () => {
     console.log("EduBot Online!");
-    channelsID = getChannelIDs(channelsPath);
+    channelsID = new Set(load_channels(channelsPath));
     setInterval(checkAnnounement, checkInterval);
     checkAnnounement();
-    console.log(channelsID);
 })
 
-startBot();
+checkDirIntegrity().then((resolvemsg) => {
+    console.log(resolvemsg);
+    startBot();
+}, (errormsg) => console.error(errormsg));
